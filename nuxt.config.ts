128e1783d407954cf0ce630b3570f5c838cd5dd7
Dodi Yulian <cdoditri@gmail.com>
2022-12-07 20:55:10 +0700

    Metadata & header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt Dojo App By Dodi',
+      meta: [
+        { name: 'description', content: 'Everything about Nuxt 3' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      ]
+    }
+  }
 })
